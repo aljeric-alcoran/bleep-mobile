f4547af633bb0b38778938676b8af9804570cc12
AL <alcoran026@gmail.com>
2025-11-11 12:38:11 +0800

    [spartan-setup] Install spartan/ui

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, HlmButtonImports],
 })
 export class HomePage {
   constructor() {}
