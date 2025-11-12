585315364c3c91fd7c4541514a3a25542a28447f
AL <alcoran026@gmail.com>
2025-11-12 14:26:32 +0800

    [login] Install spartan ui components like alert

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
    {
       path: 'home',
-      loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+      loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
    },
    {
       path: 'login',
-      loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent),
+      loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginComponent),
    },
    {
       path: '',
