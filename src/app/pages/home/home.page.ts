43009f336740c91b370f76b5fc5d24473d5e95fd
AL <alcoran026@gmail.com>
2025-11-12 15:25:57 +0800

    [login] Added temp design for home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonContent, IonFooter } from '@ionic/angular/standalone';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmInputImports } from '@spartan-ng/helm/input';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { 
+   lucideSearch, 
+   lucideShoppingCart, 
+   lucideMessageCircle,
+   lucideHouse,
+   lucideCircleUserRound,
+   lucideShoppingBag,
+   lucideTv,
+   lucideBell
+} from '@ng-icons/lucide';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, HlmButtonImports],
+   selector: 'app-home',
+   templateUrl: 'home.page.html',
+   styleUrls: ['home.page.scss'],
+   providers: [provideIcons({ 
+      lucideSearch, 
+      lucideShoppingCart, 
+      lucideMessageCircle,
+      lucideHouse,
+      lucideCircleUserRound,
+      lucideShoppingBag,
+      lucideTv,
+      lucideBell
+   })],
+   imports: [ 
+      NgIcon,
+      IonHeader, 
+      IonToolbar, 
+      IonContent,
+      IonFooter, 
+      HlmInputImports, 
+      HlmButtonImports
+   ],
 })
 export class HomePage {
-  constructor() {}
+   constructor() {}
 }
