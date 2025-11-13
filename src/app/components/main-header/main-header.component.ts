8110fe3e410a5647d5595a50453e0d3525b65358
AL <alcoran026@gmail.com>
2025-11-13 11:06:46 +0800

    [login] Added a main-header component

---
+++ src/app/components/main-header/main-header.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar } from '@ionic/angular/standalone';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmInputImports } from '@spartan-ng/helm/input';
+import { 
+   lucideSearch, 
+   lucideShoppingCart, 
+   lucideMessageCircle,
+} from '@ng-icons/lucide';
+
+@Component({
+   selector: 'app-main-header',
+   templateUrl: './main-header.component.html',
+   styleUrls: ['./main-header.component.scss'],
+   providers: [provideIcons({ 
+      lucideSearch,
+      lucideShoppingCart,
+      lucideMessageCircle 
+   })],
+   imports: [
+      NgIcon,
+      IonHeader,
+      IonToolbar,
+      HlmButtonImports,
+      HlmInputImports
+   ]
+})
+export class MainHeaderComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
