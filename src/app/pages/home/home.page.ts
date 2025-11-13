60f0d863765b68baa41661dbe20bc9c314897b82
AL <alcoran026@gmail.com>
2025-11-13 11:07:18 +0800

    [login] Implement the app main header component

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,32 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonContent } from '@ionic/angular/standalone';
-import { HlmButtonImports } from '@spartan-ng/helm/button';
-import { HlmInputImports } from '@spartan-ng/helm/input';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { 
-   lucideSearch, 
-   lucideShoppingCart, 
-   lucideMessageCircle,
-} from '@ng-icons/lucide';
+import { IonContent } from '@ionic/angular/standalone';
 import { FooterMenuComponent } from "src/app/components/footer-menu/footer-menu.component";
+import { MainHeaderComponent } from "src/app/components/main-header/main-header.component";
 
 @Component({
    selector: 'app-home',
    templateUrl: 'home.page.html',
    styleUrls: ['home.page.scss'],
-   providers: [provideIcons({ 
-      lucideSearch, 
-      lucideShoppingCart, 
-      lucideMessageCircle,
-   })],
    imports: [
-      NgIcon,
-      IonHeader,
-      IonToolbar,
       IonContent,
-      HlmInputImports,
-      HlmButtonImports,
-      FooterMenuComponent
+      FooterMenuComponent,
+      MainHeaderComponent
    ],
 })
 export class HomePage {
