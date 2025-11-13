f756eca8166f7753787f297c02eee45f05cd8330
AL <alcoran026@gmail.com>
2025-11-13 10:31:19 +0800

    [login] Added a footer menu component

---
+++ src/app/components/footer-menu/footer-menu.component.ts
@@ -0,0 +1,48 @@
+import { Component, computed, OnInit } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+   lucideHouse,
+   lucideCircleUserRound,
+   lucideShoppingBag,
+   lucideTv,
+   lucideBell
+} from '@ng-icons/lucide';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+@Component({
+   selector: 'app-footer-menu',
+   templateUrl: './footer-menu.component.html',
+   styleUrls: ['./footer-menu.component.scss'],
+   providers: [provideIcons({
+      lucideHouse,
+      lucideCircleUserRound,
+      lucideShoppingBag,
+      lucideTv,
+      lucideBell
+   })],
+   imports: [
+      NgIcon
+   ]
+})
+export class FooterMenuComponent  implements OnInit {
+   menuList = [
+      { label: 'Home', icon: 'lucideHouse', route: '/home' },
+      { label: 'Mall', icon: 'lucideShoppingBag', route: '/mall' },
+      { label: 'Live & Videos', icon: 'lucideTv', route: '/live-and-videos' },
+      { label: 'Notifications', icon: 'lucideBell', route: '/notifications' },
+      { label: 'Profile', icon: 'lucideCircleUserRound', route: '/profile' },
+   ];
+   activeRoute!: string;
+
+   constructor(private router: Router) {
+      this.router.events
+         .pipe(filter(event => event instanceof NavigationEnd))
+         .subscribe((event: NavigationEnd) => {
+            console.log('Now on:', event.urlAfterRedirects);
+            this.activeRoute = event.urlAfterRedirects
+         });
+   }
+
+   ngOnInit() {}
+}
