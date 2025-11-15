d387d01b8c7d45484ad91a1a73f88615f6d9c594
AL <alcoran026@gmail.com>
2025-11-15 22:52:23 +0800

    [login] Implement login using service

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmInputImports } from '@spartan-ng/helm/input';
 import { HlmLabelImports } from '@spartan-ng/helm/label';
@@ -10,6 +10,8 @@ import {
    ReactiveFormsModule,
    Validators
 } from '@angular/forms';
+import { LoginService } from 'src/app/services/login.service';
+import { debounceTime } from 'rxjs';
 
 @Component({
    selector: 'app-login-form',
@@ -24,27 +26,53 @@ import {
       AlertComponent,
    ]
 })
-export class LoginFormComponent {
-   private readonly _formBuider = inject(FormBuilder);
-   public formGroup: FormGroup = this._formBuider.group({
+export class LoginFormComponent implements OnInit {
+   private loginService = inject(LoginService);
+   private destroyRef = inject(DestroyRef);
+   private readonly formBuider = inject(FormBuilder);
+   public formGroup: FormGroup = this.formBuider.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
-   })
+   });
+   
    hasError = signal<boolean>(false);
+   errorMessage = signal<string>('');
 
    constructor() {}
 
+   ngOnInit(): void {
+      Object.keys(this.formGroup.controls).forEach(controlName => {
+         this.formGroup.get(controlName)?.valueChanges
+         .pipe(debounceTime(300))
+         .subscribe(() => {
+            this.hasError.set(false);
+         });
+      });
+   }
+
    isFormInvalid(formControlName: string, error: string) {
       return (
-        this.formGroup.controls[formControlName].touched &&
-        this.formGroup.controls[formControlName].invalid &&
-        this.formGroup.controls[formControlName].hasError(error)
+         this.formGroup.controls[formControlName].touched &&
+         this.formGroup.controls[formControlName].invalid &&
+         this.formGroup.controls[formControlName].hasError(error)
       );
-    }
+   }
 
    onSubmit() {
       if (this.formGroup.invalid) return;
       const payload = this.formGroup.value;
-      console.log(payload);
+      const subscription = this.loginService.onLoginUser(payload).subscribe({
+         next: (response) => {
+            console.log(response);
+         },
+         error: (error) => {
+            this.hasError.set(true);
+            this.errorMessage.set(error?.message);
+         }
+      });
+
+      this.destroyRef.onDestroy(() => {
+         subscription.unsubscribe();
+      })
    }
 }
